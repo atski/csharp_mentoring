f5be170aa00efadc98858b05b366d50f8fc4bd7c
viktormelnyk <viktormelnyk@NB00GLO017>
2018-11-21 23:10:22 +0200

    tets pull request

---
+++ ConsoleApp1/FileCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class RandomNumbersFileCreate
+    {
+        private Array GenerateNumbers(int numbersQuantity)
+        {
+            Random randGen = new Random();
+            int[] numbersArray = new int[numbersQuantity];
+            for (int i = 0; i < numbersQuantity; i++)
+            {
+                numbersArray[i] = randGen.Next(numbersQuantity);
+            }
+            return numbersArray;
+        }
+
+        public void CreateFile(string path, int numbers)
+        {
+            var randomNumbers = GenerateNumbers(numbers);
+
+            if (File.Exists(path)){
+                File.Delete(path);
+            }
+            else
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach(int i in randomNumbers)
+                    {
+                        sw.WriteLine("{0}", i);
+                    }
+                }
+            }
+
+        }
+
+       
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileCreate.cs;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int elementsNumber = 0;
+
+            Console.WriteLine("Enter desired path to generated files:");
+            string filesPath = Console.ReadLine();
+            Console.WriteLine("enter nuber of records (<1bill)");
+            if (!Int32.TryParse(Console.ReadLine(), out elementsNumber)) {
+                Console.WriteLine("not integer");
+            }
+        
+
+        }
+    }
+}
